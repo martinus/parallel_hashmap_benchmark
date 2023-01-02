94eba7eef932f98d8da82022b79c5b625437ec00
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2023-01-02 09:14:19 +0100

    build command

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-ccache clang++ --std=c++17 -ltbb -I $HOME/git/libcuckoo/libcuckoo -I $HOME/git/boostorg_unordered/include -I $HOME/dev/boost_1_81_0/ -O3 parallel_maps_test.cpp
+clang++ --std=c++17 -I $HOME/git/boostorg_unordered/include -I $HOME/dev/boost_1_81_0/ -O3 -march=native random_frequencies.cpp
